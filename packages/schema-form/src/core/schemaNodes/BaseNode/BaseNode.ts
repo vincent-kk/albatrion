ed6fd26ffb6b122a89061d5a6f6014cf6ce57385
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-14 05:02:30 +0900

    base node 코드 일부 수정, 타입 개선

--- packages/schema-form/src/core/schemaNodes/BaseNode/BaseNode.ts
+++ packages/schema-form/src/core/schemaNodes/BaseNode/BaseNode.ts
@@ -159,109 +159,51 @@ export abstract class BaseNode {
     }
   };
 
-  findNode = <T extends 'union' | 'base' = 'union'>(
-    path: string,
-  ): SchemaNode<T> | null => find(this, path);
+  findNode(path: string) {
+    return find(this, path) as SchemaNode | null;
+  }
 
-  subscribe = (callback: Listener) => {
+  subscribe(callback: Listener) {
     this._listeners.push(callback);
     return () => {
       this._listeners = this._listeners.filter(
         (listener) => listener !== callback,
       );
     };
-  };
-  publish = <T extends MethodType>(type: T, payload: MethodPayload[T]) => {
+  }
+  publish<T extends MethodType>(type: T, payload: MethodPayload[T]) {
     this._listeners.forEach((listener) => listener(type, payload));
-  };
+  }
 
-  filterErrorsWithSchema = (errors: JsonSchemaError[]) => {
+  filterErrorsWithSchema(errors: JsonSchemaError[]) {
     if (!this.isRoot) {
       return errors;
     }
-    const visibleJsonPaths = getJsonPaths(
-      getDataWithSchema(this.getValue(), this.schema),
+    const visibleJsonPaths = new Set(
+      getJsonPaths(getDataWithSchema(this.getValue(), this.jsonSchema)),
     );
-    return errors.filter(({ dataPath }) => visibleJsonPaths.includes(dataPath));
-  };
+    return errors.filter(({ dataPath }) => visibleJsonPaths.has(dataPath));
+  }
 
-  protected _listeners: Listener[] = [];
-  protected _validate?: Function;
+  #validate: ValidateFunction | null = null;
 
-  private _errors: JsonSchemaError[] = [];
-  private _errorHash?: string;
-  private _errorDataPaths: string[] = [];
-
-  private _receivedErrors: JsonSchemaError[] = [];
-  private _receivedErrorHash?: string;
-
-  private _state: NodeState = {};
-
-  constructor({
-    key,
-    name,
-    schema,
-    defaultValue,
-    parentNode,
-    ajv,
-  }: ConstructorProps) {
-    this.parentNode = parentNode || null;
-    // NOTE: BaseNode 자체를 사용하는 경우는 없으므로, this는 SchemaNode
-    this.rootNode = (this.parentNode?.rootNode || this) as SchemaNode;
-    this.isRoot = !this.parentNode;
-    this.isArrayItem = this.parentNode?.schema?.type === 'array';
-    this.name = name || '';
-    this.path = this.parentNode?.getPath()
-      ? `${this.parentNode.getPath()}${JSONPath.Child}${this.getName()}`
-      : JSONPath.Root;
-    this.key = this.parentNode?.getPath()
-      ? `${this.parentNode.getPath()}${JSONPath.Child}${
-          typeof key === 'undefined' ? this.getName() : key
-        }`
-      : JSONPath.Root;
-
-    this.depth = this.path.split(JSONPath.Child).filter(Boolean).length - 1;
-    this.schema = schema;
-    this.defaultValue = defaultValue;
-
-    if (this.parentNode) {
-      this.parentNode.subscribe((type) => {
-        if (type === MethodType.PathChange) {
-          this.updatePath();
-        }
-      });
-    }
-
-    if (this.isRoot) {
-      const ajvInstance =
-        ajv || new Ajv({ allErrors: true, unknownFormats: 'ignore' });
-      let ajvValidate: ValidateFunction;
-      try {
-        ajvValidate = ajvInstance.compile({ ...schema, $async: true });
+  async validate(value: Value) {
+    if (!this.isRoot || !this.#validate) return [];
+    try {
+      await this.#validate(value);
       } catch (err: any) {
-        ajvValidate = async () => {
-          throw {
-            errors: [
-              { keyword: '__jsonSchema', parent: {}, message: err.message },
-            ],
-          };
-        };
-      }
-
-      const validate = async (value: any) => {
-        let nextErrors = [];
-        try {
-          await ajvValidate(value);
-        } catch (err: any) {
-          nextErrors = err.errors;
+      return err.errors as JsonSchemaError[];
         }
-        return nextErrors;
-      };
+    return [];
+  }
 
-      const validateOnChange = async () => {
+  async validateOnChange() {
+    // NOTE: 루트 노드가 아니면 검증 수행 안함
+    if (!this.isRoot) return;
+
         // NOTE: 1. 현재 Form 내의 value와 schema를 이용해서 validation 수행
         const errors = filterErrors(
-          await validate(getDataWithSchema(this.getValue(), schema)),
+      await this.validate(getDataWithSchema(this.getValue(), this.jsonSchema)),
         );
         // NOTE: 2. 얻어진 error들을 dataPath 별로 분류
         const errorsByDataPath = transformErrors(errors).reduce(
@@ -291,141 +233,77 @@ export abstract class BaseNode {
             node?.clearErrors();
           });
         this._errorDataPaths = _errorDataPaths;
-      };
+  }
+
+  constructor({
+    key,
+    name,
+    jsonSchema,
+    defaultValue,
+    parentNode,
+    ajv,
+  }: ConstructorProps<Value, Schema>) {
+    this.jsonSchema = jsonSchema;
+    this.defaultValue = defaultValue;
+    this.parentNode = parentNode || null;
+
+    // NOTE: BaseNode 자체를 사용하는 경우는 없으므로, this는 SchemaNode
+    this.rootNode = (this.parentNode?.rootNode || this) as SchemaNode;
+    this.isRoot = !this.parentNode;
+    this.isArrayItem = this.parentNode?.jsonSchema?.type === 'array';
+    this.name = name || '';
+
+    this.path = this.parentNode?.getPath()
+      ? `${this.parentNode.getPath()}${JSONPath.Child}${this.getName()}`
+      : JSONPath.Root;
+
+    this.key = this.parentNode?.getPath()
+      ? `${this.parentNode.getPath()}${JSONPath.Child}${
+          typeof key === 'undefined' ? this.getName() : key
+        }`
+      : JSONPath.Root;
+
+    this.depth = this.path.split(JSONPath.Child).filter(isTruthy).length - 1;
+
+    if (this.parentNode) {
+      this.parentNode.subscribe((type) => {
+        if (type === MethodType.PathChange) {
+          this.updatePath();
+        }
+      });
+    }
+
+    if (this.isRoot) {
+      const ajvInstance = ajv || new Ajv({ allErrors: true });
+      try {
+        this.#validate = ajvInstance.compile({ ...jsonSchema, $async: true });
+      } catch (err: any) {
+        this.#validate = Object.assign(
+          (_: any): _ is unknown => {
+            throw {
+              errors: [
+                { keyword: '__jsonSchema', parent: {}, message: err.message },
+              ],
+            };
+          },
+          {
+            errors: null,
+            schema: jsonSchema, // AnySchema 타입에 맞는 스키마 객체 필요
+            schemaEnv: {} as any,
+          },
+        );
+      }
 
       this.subscribe((type) => {
         if (type === MethodType.Change) {
-          validateOnChange();
+          this.validateOnChange();
         }
       });
 
       // validate for initial value
       setTimeout(() => {
-        validateOnChange();
+        this.validateOnChange();
       }, 0);
     }
   }
 }
-
-function find(target: SchemaNode | BaseNode, path: string): SchemaNode | null {
-  const [currPath, ...rest] = path
-    .replace(/^\.([^.])/, '$1')
-    // to access array items
-    .replace(/\[(\d+)\]/g, '.$1')
-    .split(JSONPath.Child);
-  if (path === '') {
-    return target as SchemaNode;
-  } else if (currPath === JSONPath.Root) {
-    return find(target?.rootNode, rest.join(JSONPath.Child));
-  } else if (currPath === JSONPath.Current) {
-    return find(target?.parentNode!, rest.join(JSONPath.Child));
-  } else if (target?.children) {
-    const children = target.children();
-    const found = children.find((e) => e.node.getName() === currPath);
-    if (found && rest.length > 0) {
-      return find(found.node, rest.join(JSONPath.Child));
-    }
-    return found?.node || null;
-  }
-  return null;
-}
-
-let keySeq = 0;
-
-export const transformErrors = (
-  errors: JsonSchemaError[],
-  useKey = false,
-): JsonSchemaError[] => {
-  return (Array.isArray(errors) ? errors : []).map((error) => {
-    let key = useKey ? ++keySeq : undefined;
-    if (
-      typeof error.dataPath === 'string' &&
-      error.keyword === 'required' &&
-      error.params?.missingProperty
-    ) {
-      return {
-        ...error,
-        key,
-        dataPath: `${error.dataPath ? `${error.dataPath}.` : ''}${
-          error.params.missingProperty
-        }`,
-      };
-    }
-    return { ...error, key };
-  }, []);
-};
-
-const getDataWithSchema = (
-  data: any,
-  schema: Schema,
-  options?: { ignoreAnyOf: boolean },
-): any => {
-  let node = { ...schema };
-  if (
-    node.type === 'object' &&
-    typeof node.properties === 'object' &&
-    data &&
-    typeof data === 'object' &&
-    !Array.isArray(data)
-  ) {
-    let items: any[];
-    let omit: string[] = [];
-    const { anyOf } = node;
-    if (
-      options?.ignoreAnyOf !== true &&
-      Array.isArray(anyOf) &&
-      anyOf.length > 0
-    ) {
-      const required: string[] = [...(node.required || [])];
-      const notRequired: string[] = [];
-      anyOf
-        .filter(
-          (item: any) => item?.properties && Array.isArray(item?.required),
-        )
-        .forEach(({ properties, required: _required }: any) => {
-          const key = Object.keys(properties || {})[0];
-          if ((properties?.[key]?.enum || []).includes(data?.[key])) {
-            return required.push(..._required);
-          }
-          return notRequired.push(..._required);
-        });
-      omit = notRequired.filter((field) => !required.includes(field));
-    }
-    try {
-      items = Object.entries(node.properties).map(([k, v]) => {
-        if (k in data && !omit.includes(k)) {
-          return [k, getDataWithSchema(data[k], v, options)];
-        }
-        return false;
-      });
-    } catch (err: any) {
-      throw err;
-    }
-    return Object.fromEntries(items.filter(Boolean));
-  } else if (
-    node.type === 'array' &&
-    typeof node.items === 'object' &&
-    Array.isArray(data)
-  ) {
-    return data.map((e) => getDataWithSchema(e, node.items, options));
-  }
-  return data;
-};
-
-const getJsonPaths = (data: any, jsonPath = '') => {
-  const jsonPaths = [jsonPath];
-  if (Array.isArray(data)) {
-    jsonPaths.push(
-      ...data
-        .map((value, i) => getJsonPaths(value, `${jsonPath}[${i}]`))
-        .flat(),
-    );
-  } else if (data && typeof data === 'object') {
-    jsonPaths.push(
-      ...Object.entries(data)
-        .map(([name, value]) => getJsonPaths(value, `${jsonPath}.${name}`))
-        .flat(),
-    );
-  }
-  return jsonPaths.filter(Boolean);
-};
