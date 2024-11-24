466124371356318742c84e0b47c21678c1426186
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-24 15:15:27 +0900

    FormRefHandle를 테스트하는 스토리 추가

--- packages/schema-form/stories/Form.stories.tsx
+++ packages/schema-form/stories/Form.stories.tsx
@@ -1,9 +1,10 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 
 import Form, {
-  FormTypeInputDefinition,
-  FormTypeInputMap,
-  FormTypeInputProps,
+  type FormHandle,
+  type FormTypeInputDefinition,
+  type FormTypeInputMap,
+  type FormTypeInputProps,
   type JsonSchema,
 } from '../src';
 
@@ -237,6 +238,124 @@ export const FormTypeMap = () => {
     <div>
       <Form
         jsonSchema={schema}
+        formTypeInputMap={formTypeMap}
+        onChange={handleChange}
+      />
+      <pre>{JSON.stringify(value, null, 2)}</pre>
+    </div>
+  );
+};
+
+export const FormRefHandle = () => {
+  const [value, setValue] = useState({});
+  const schema = {
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      number: {
+        type: 'number',
+      },
+      objectNode: {
+        type: 'object',
+        properties: {
+          test: { type: 'string' },
+        },
+      },
+    },
+  } satisfies JsonSchema;
+  const defaultValue = useRef({
+    name: 'ron',
+    number: 10,
+  });
+
+  const formTypeMap = useMemo<FormTypeInputMap>(() => {
+    return {
+      '$.objectNode': ({ onChange }: FormTypeInputProps<{ test?: string }>) => {
+        const handleClick = () => {
+          onChange({ test: 'wow' });
+        };
+        const handleUnsetClick = () => {
+          onChange({}, { replace: true });
+        };
+        return (
+          <div>
+            <button onClick={handleClick}>object set</button>
+            <button onClick={handleUnsetClick}>object unset</button>
+          </div>
+        );
+      },
+      '$.textNode': ({ onChange }: FormTypeInputProps) => {
+        const handleClick = () => {
+          onChange('wow');
+        };
+        return <button onClick={handleClick}>text set</button>;
+      },
+      '$.arrayNode.#': () => {
+        return <div>i am array item</div>;
+      },
+    };
+  }, []);
+
+  const formHandle = useRef<
+    FormHandle<
+      typeof schema,
+      {
+        name?: string;
+        number?: number;
+      }
+    >
+  >(null);
+
+  const handleChange = (val: any) => {
+    setValue(val);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          formHandle.current?.setValue({
+            name: 'harry',
+          })
+        }
+      >
+        set name
+      </button>
+      <button
+        onClick={() =>
+          formHandle.current?.setValue((prev) => {
+            return {
+              number: (prev?.number ?? 0) + 1,
+            };
+          })
+        }
+      >
+        increase number
+      </button>
+      <button
+        onClick={() =>
+          formHandle.current?.setValue(
+            {
+              number: 100,
+            },
+            { replace: true },
+          )
+        }
+      >
+        overwrite number
+      </button>
+      <button onClick={() => formHandle.current?.reset()}>reset</button>
+      <button
+        onClick={() => {
+          formHandle.current?.reset({ name: 'hermione', number: 12 });
+        }}
+      >
+        change defaultValue
+      </button>
+      <Form
+        ref={formHandle}
+        jsonSchema={schema}
+        defaultValue={defaultValue.current}
         formTypeInputMap={formTypeMap}
         onChange={handleChange}
       />
