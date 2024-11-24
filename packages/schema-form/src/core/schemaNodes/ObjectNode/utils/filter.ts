bdc18f80682448522bfbcc465d806d01e4a48dda
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-24 17:55:31 +0900

    getOneOfConditionsMap 추가, 기존 anyOf 코드 무시

--- packages/schema-form/src/core/schemaNodes/ObjectNode/utils/filter.ts
+++ packages/schema-form/src/core/schemaNodes/ObjectNode/utils/filter.ts
@@ -7,6 +7,11 @@ export const isObjectAnyOfSchema = (
 ): schema is RequiredBy<ObjectSchema, 'properties' | 'required'> =>
   isPlainObject(schema.properties) && Array.isArray(schema.required);
 
+export const isObjectOneOfSchema = (
+  schema: NonNullable<ObjectSchema['oneOf']>[number],
+): schema is RequiredBy<ObjectSchema, 'properties' | 'required'> =>
+  isPlainObject(schema.properties) && Array.isArray(schema.required);
+
 export const isValidEnum = (
   entire: [key: string, value: JsonSchema],
 ): entire is [key: string, value: RequiredBy<JsonSchema, 'enum'>] =>
