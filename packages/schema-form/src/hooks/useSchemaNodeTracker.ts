386b99f17fae1bac51c964b63f7cab06223f514f
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-24 12:58:33 +0900

    useSchemaNodeTracker 코드 간소화

--- packages/schema-form/src/hooks/useSchemaNodeTracker.ts
+++ packages/schema-form/src/hooks/useSchemaNodeTracker.ts
@@ -1,7 +1,6 @@
 import { useLayoutEffect } from 'react';
 
 import type { SchemaNode } from '@lumy/schema-form/core';
-import { isFunction } from '@lumy/schema-form/helpers/filter';
 
 import { useTick } from './useTick';
 
@@ -10,12 +9,7 @@ export function useSchemaNodeTracker<Node extends SchemaNode>(
 ) {
   const [tick, update] = useTick();
   useLayoutEffect(() => {
-    const unsubscribe =
-      node && isFunction(node.subscribe)
-        ? node.subscribe(() => {
-            update();
-          })
-        : null;
+    const unsubscribe = node?.subscribe(update);
     return () => {
       unsubscribe?.();
     };
