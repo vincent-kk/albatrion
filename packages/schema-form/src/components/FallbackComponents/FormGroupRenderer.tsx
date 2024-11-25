66d6901b1b2e3bf441b93a4c1357e3eaaa75aa7e
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-25 21:32:53 +0900

    FormGroupRenderer일때, depth === 0 이면 Input만 표시

--- packages/schema-form/src/components/FallbackComponents/FormGroupRenderer.tsx
+++ packages/schema-form/src/components/FallbackComponents/FormGroupRenderer.tsx
@@ -1,16 +1,20 @@
 import type { FormTypeRendererProps } from '@lumy/schema-form/types';
 
 export const FormGroupRenderer = ({
+  depth,
   name,
   Input,
   errorMessage,
-}: FormTypeRendererProps) => (
-  <div>
-    <label>
-      <span>{name}</span>
-      <Input />
-    </label>
-    <br />
-    <em>{errorMessage}</em>
-  </div>
-);
+}: FormTypeRendererProps) =>
+  depth ? (
+    <div>
+      <label>
+        <span>{name}</span>
+        <Input />
+      </label>
+      <br />
+      <em>{errorMessage}</em>
+    </div>
+  ) : (
+    <Input />
+  );
