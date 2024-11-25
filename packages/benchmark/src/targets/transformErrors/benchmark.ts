82171af9be2a93162a38ad57206028557a7602db
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-26 03:11:17 +0900

    transformErrors 벤치마크 개선

--- packages/benchmark/src/targets/transformErrors/benchmark.ts
+++ packages/benchmark/src/targets/transformErrors/benchmark.ts
@@ -1,25 +1,35 @@
 import Benchmark from 'benchmark';
 
 import { type Ratio, getRatio } from '@lumy/benchmark/helpers/getRatio';
-import { transformErrors } from '@lumy/schema-form/core/schemaNodes/BaseNode/utils';
+import { transformErrors as transformErrors_Ref } from '@lumy/schema-form/core/schemaNodes/BaseNode/utils';
 
 import { ajvErrors } from './data';
+import { transformErrors } from './transformErrors';
 import { transformErrors as transformErrors_Old } from './transformErrors_old';
 
 const suite = new Benchmark.Suite();
 
+const error1 = JSON.parse(JSON.stringify(ajvErrors));
 const testTransformErrors = () => {
-  transformErrors(ajvErrors);
+  transformErrors(error1, true);
 };
+
+const error2 = JSON.parse(JSON.stringify(ajvErrors));
 const testTransformErrors_Old = () => {
-  transformErrors_Old(ajvErrors);
+  transformErrors_Old(error2, true);
+};
+
+const error3 = JSON.parse(JSON.stringify(ajvErrors));
+const testTransformErrors_Ref = () => {
+  transformErrors_Ref(error3, true);
 };
 
 export const run = () => {
   return new Promise<Ratio>((resolve) => {
     suite
-      .add('transformErrors', testTransformErrors)
       .add('transformErrors_Old', testTransformErrors_Old)
+      .add('transformErrors', testTransformErrors)
+      .add('transformErrors_Ref', testTransformErrors_Ref)
       .on('cycle', function (event: Benchmark.Event) {
         console.log(String(event.target));
       })
