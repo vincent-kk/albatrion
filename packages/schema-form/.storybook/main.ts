d40e82bdbf40f7f56d9b7e47558c916544b149c5
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-28 23:51:39 +0900

    onboarding 제거

--- packages/schema-form/.storybook/main.ts
+++ packages/schema-form/.storybook/main.ts
@@ -18,7 +18,6 @@ const config: StorybookConfig = {
     '../coverage/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
