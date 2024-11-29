2883492de5d18e0d69fd31c4a6372674345e4969
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-29 18:28:40 +0900

    null schema stroy 작성

---
+++ packages/schema-form/coverage/NullSchema.stories.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import Form, {
+  type JsonSchema,
+  JsonSchemaError,
+} from '@lumy-pack/schema-form/src';
+
+import StoryLayout from './components/StoryLayout';
+
+export default {
+  title: 'Form/9. NullSchema',
+};
+
+export const NullSchema = () => {
+  const jsonSchema = {
+    type: 'object',
+    properties: {
+      string: {
+        type: 'string',
+      },
+      null: {
+        type: 'null',
+        nullable: true,
+      },
+      defaultNull: {
+        type: 'null',
+        nullable: true,
+        default: null,
+      },
+    },
+  } satisfies JsonSchema;
+
+  const [value, setValue] = useState<Record<string, unknown>>({});
+  const [errors, setErrors] = useState<JsonSchemaError[]>([]);
+
+  return (
+    <StoryLayout jsonSchema={jsonSchema} errors={errors} value={value}>
+      <Form
+        jsonSchema={jsonSchema}
+        defaultValue={{ null: null }}
+        onChange={setValue}
+        onError={setErrors}
+      />
+    </StoryLayout>
+  );
+};
