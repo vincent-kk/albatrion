410384e1f50acab568f565ee431559ef80521839
Vincent K. Kelvin <lunox273@gmail.com>
2024-11-29 18:26:48 +0900

    NullNode 추가

---
+++ packages/schema-form/src/core/nodes/NullNode/NullNode.ts
@@ -0,0 +1,55 @@
+import type { NullSchema, NullValue } from '@lumy/schema-form/types';
+
+import { BaseNode } from '../BaseNode';
+import { MethodType, type SchemaNodeConstructorProps } from '../type';
+
+export class NullNode extends BaseNode<NullSchema, NullValue> {
+  #value: NullValue | undefined;
+  get value() {
+    return this.#value;
+  }
+  set value(input: NullValue | undefined) {
+    this.setValue(input);
+  }
+  protected applyValue(input: NullValue | undefined) {
+    this.#emitChange(input);
+  }
+  parseValue(input: NullValue | undefined) {
+    return input;
+  }
+  #emitChange(input: NullValue | undefined) {
+    const previous = this.#value;
+    const current = this.parseValue(input);
+    if (previous !== current) {
+      this.#value = current;
+      this.onChange(current);
+      this.publish({
+        type: MethodType.Change,
+        payload: current,
+        options: {
+          previous,
+          current,
+        },
+      });
+    }
+  }
+  constructor({
+    key,
+    name,
+    jsonSchema,
+    defaultValue,
+    onChange,
+    parentNode,
+    ajv,
+  }: SchemaNodeConstructorProps<NullSchema>) {
+    super({ key, name, jsonSchema, defaultValue, onChange, parentNode, ajv });
+
+    if (defaultValue !== undefined) {
+      this.setValue(defaultValue);
+    }
+    if (jsonSchema.default !== undefined) {
+      this.setDefaultValue(this.parseValue(jsonSchema.default));
+      this.#emitChange(this.defaultValue);
+    }
+  }
+}
