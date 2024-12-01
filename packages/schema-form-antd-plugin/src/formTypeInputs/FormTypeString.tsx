687bd60875ab55aae593cdea1e76a8a049a70d13
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-01 23:25:22 +0900

    FormTypeString 추가

---
+++ packages/schema-form-antd-plugin/src/formTypeInputs/FormTypeString.tsx
@@ -0,0 +1,58 @@
+import { type ChangeEvent, useMemo } from 'react';
+
+import { Input } from 'antd';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+
+import { useHandle } from '@lumy-pack/common-react';
+import type {
+  FormTypeInputDefinition,
+  FormTypeInputPropsWithSchema,
+  StringSchema,
+} from '@lumy-pack/schema-form';
+
+interface StringJsonSchema extends StringSchema {
+  format?: 'password';
+  formType?: 'password';
+}
+
+const FormTypeString = ({
+  path,
+  name,
+  jsonSchema,
+  readOnly,
+  disabled,
+  defaultValue,
+  onChange,
+  context,
+}: FormTypeInputPropsWithSchema<
+  string,
+  StringJsonSchema,
+  { size?: SizeType }
+>) => {
+  const TextInput = useMemo(() => {
+    if (jsonSchema.format === 'password' || jsonSchema.formType === 'password')
+      return Input.Password;
+    else return Input;
+  }, [jsonSchema]);
+  const handleChange = useHandle((event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  });
+  return (
+    <TextInput
+      id={path}
+      name={name}
+      readOnly={readOnly}
+      disabled={disabled}
+      defaultValue={defaultValue}
+      onChange={handleChange}
+      size={context?.size}
+    />
+  );
+};
+
+export const FormTypeStringDefinition = {
+  Component: FormTypeString,
+  test: {
+    type: 'string',
+  },
+} satisfies FormTypeInputDefinition;
