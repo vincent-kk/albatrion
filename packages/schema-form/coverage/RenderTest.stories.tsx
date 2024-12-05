b4fcac06e9219154dfa45ccf2c4e2081d629e7b6
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-05 20:30:53 +0900

    fix stories and add RenderTest Story

---
+++ packages/schema-form/coverage/RenderTest.stories.tsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from 'react';
+
+import { Form, JsonSchema } from '@lumy-pack/schema-form/src';
+
+import StoryLayout from './components/StoryLayout';
+
+export default {
+  title: 'Form/12. RenderTest',
+};
+
+export const FunctionalChildren = () => {
+  const jsonSchema = {
+    type: 'object',
+    properties: {
+      allowed: {
+        type: 'boolean',
+      },
+      name: {
+        type: 'string',
+      },
+      age: {
+        type: 'number',
+      },
+      gender: {
+        type: 'string',
+        enum: ['male', 'female'],
+      },
+    },
+  } satisfies JsonSchema;
+
+  const defaultValue = useRef<Record<string, any>>({
+    allowed: false,
+  });
+
+  const [value, setValue] = useState<Record<string, unknown>>();
+
+  return (
+    <StoryLayout jsonSchema={jsonSchema} value={value}>
+      <Form
+        jsonSchema={jsonSchema}
+        defaultValue={defaultValue.current}
+        onChange={setValue}
+      >
+        <div>
+          INPUT
+          <Form.Input
+            path=".allowed"
+            name="allowed"
+            style={{ color: 'red' }}
+            context={{ a: 1 }}
+          />
+        </div>
+        <div>
+          <Form.Render path=".name">
+            {({ Input, node }) => {
+              return (
+                <div>
+                  {node.name}
+                  <Input />
+                </div>
+              );
+            }}
+          </Form.Render>
+        </div>
+        <div>
+          <Form.Label path=".age" />
+          <Form.Input path=".age" />
+        </div>
+        <div>
+          <Form.Label path=".gender" />
+          <Form.Input path=".gender" />
+        </div>
+      </Form>
+    </StoryLayout>
+  );
+};
