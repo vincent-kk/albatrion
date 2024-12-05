6475516616b8cd47ec5fd7d2ce6713670ca35b18
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-06 02:47:25 +0900

    상수 객체값 반영

--- packages/schema-form/src/providers/FormTypeInputsContext/FormTypeInputsContext.ts
+++ packages/schema-form/src/providers/FormTypeInputsContext/FormTypeInputsContext.ts
@@ -1,5 +1,7 @@
 import { createContext } from 'react';
 
+import { EMPTY_ARRAY } from '@lumy-pack/common';
+
 import type { NormalizedFormTypeInputDefinition } from '@lumy/schema-form/helpers/formTypeInputDefinition';
 
 export interface FormTypeInputsContext {
@@ -7,6 +9,7 @@ export interface FormTypeInputsContext {
   fromFormTypeInputMap: NormalizedFormTypeInputDefinition[];
 }
 
-export const FormTypeInputsContext = createContext<FormTypeInputsContext>(
-  {} as FormTypeInputsContext,
-);
+export const FormTypeInputsContext = createContext<FormTypeInputsContext>({
+  fromFormTypeInputDefinitions: EMPTY_ARRAY,
+  fromFormTypeInputMap: EMPTY_ARRAY,
+} as FormTypeInputsContext);
