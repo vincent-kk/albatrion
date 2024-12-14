1944e03d9cdad4211f2aea132c54517b9e977fdc
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-14 14:23:03 +0900

    Form.Input / Form.Label / Form.Error에서 FormRenderer 제거

--- packages/schema-form/src/components/Form/components/FormLabel.tsx
+++ packages/schema-form/src/components/Form/components/FormLabel.tsx
@@ -1,34 +1,22 @@
-import { type CSSProperties, type ComponentType } from 'react';
-
-import { useMemorize } from '@lumy-pack/common-react';
+import type { CSSProperties } from 'react';
 
 import { FallbackManager } from '@/schema-form/app/FallbackManager';
 import { SchemaNodeProxy } from '@/schema-form/components/SchemaNode';
 import { useExternalFormContext } from '@/schema-form/providers';
-import type { FormTypeRendererProps } from '@/schema-form/types';
 
 export interface FormLabelProps {
   path: string;
   style?: CSSProperties;
   className?: string;
-  FormTypeRenderer?: ComponentType<FormTypeRendererProps>;
 }
 
-export const FormLabel = ({
-  path,
-  style,
-  className,
-  FormTypeRenderer: InputFormTypeRenderer,
-}: FormLabelProps) => {
+export const FormLabel = ({ path, style, className }: FormLabelProps) => {
   const { FormLabelRenderer } = useExternalFormContext();
-  const FormTypeRenderer = useMemorize(InputFormTypeRenderer);
   return (
     <label style={style} className={className} htmlFor={path}>
       <SchemaNodeProxy
         path={path}
-        FormTypeRenderer={
-          FormTypeRenderer || FormLabelRenderer || FallbackManager.FormLabel
-        }
+        FormTypeRenderer={FormLabelRenderer || FallbackManager.FormLabel}
       />
     </label>
   );
