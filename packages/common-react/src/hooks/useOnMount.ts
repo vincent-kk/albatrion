3deb8bc50d57844a9276b93e37698fc0ee0f5415
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-14 17:42:20 +0900

    useOnMount, useOnUnmount Layout 버전 추가

--- packages/common-react/src/hooks/useOnMount.ts
+++ packages/common-react/src/hooks/useOnMount.ts
@@ -1,5 +1,9 @@
-import { type EffectCallback, useEffect } from 'react';
+import { type EffectCallback, useEffect, useLayoutEffect } from 'react';
 
 export const useOnMount = (handler: EffectCallback) => {
   useEffect(handler, []);
+};
+
+export const useOnMountLayout = (handler: EffectCallback) => {
+  useLayoutEffect(handler, []);
 };
--- packages/common-react/src/hooks/useOnUnmount.ts
+++ packages/common-react/src/hooks/useOnUnmount.ts
@@ -1,7 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 
 export const useOnUnmount = (handler: Fn) => {
   useEffect(() => {
+    return handler;
+  }, []);
+};
+
+export const useOnUnmountLayout = (handler: Fn) => {
+  useLayoutEffect(() => {
     return handler;
   }, []);
 };
