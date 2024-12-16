28655892cb658e5d3fcac6e88b754b8a5371eab7
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-16 22:36:08 +0900

    사용하지 않는 코드 제거, 스토리북 수정

--- packages/promise-modal/src/helpers/emitEvent.ts
+++
@@ -1,9 +0,0 @@
-import type { ManagedModal } from '@/promise-modal/types';
-
-export const emitEvent = (
-  modalId: ManagedModal['id'],
-  dictionary: Map<ManagedModal['id'], Fn[]>,
-) => {
-  const listeners = dictionary.get(modalId) || [];
-  for (const listener of listeners) listener();
-};
