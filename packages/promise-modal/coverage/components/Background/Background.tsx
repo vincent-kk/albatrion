98c6a15ff58f8986493575e01c73700461d869af
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-17 23:33:46 +0900

    Background 애니메이션 적용

--- packages/promise-modal/coverage/components/Background/Background.tsx
+++ packages/promise-modal/coverage/components/Background/Background.tsx
@@ -1,19 +1,56 @@
-import React, { type ForwardedRef, forwardRef } from 'react';
+import React, {
+  type ForwardedRef,
+  forwardRef,
+  useLayoutEffect,
+  useRef,
+} from 'react';
 
 import {
   ModalFrameProps,
   useDestroyAfter,
   useModalDuration,
 } from '../../../src';
+// @ts-expect-error css module
+import styles from './Background.module.css';
 
 export const Background = forwardRef(
-  ({ id, background }: ModalFrameProps, ref: ForwardedRef<HTMLDivElement>) => {
+  (
+    { id, visible, background }: ModalFrameProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    const modalRef = useRef<HTMLDivElement>(null);
     const { duration } = useModalDuration();
-    useDestroyAfter(id, duration);
 
+    useLayoutEffect(() => {
+      let timer: NodeJS.Timeout | null = null;
+      if (modalRef?.current) {
+        if (visible) {
+          timer = setTimeout(() => {
+            modalRef.current?.classList.add(styles.visible);
+          });
+        } else {
+          timer = setTimeout(() => {
+            modalRef.current?.classList.remove(styles.visible);
+          });
+        }
+      }
+      return () => {
+        if (timer) clearTimeout(timer);
+      };
+    }, [visible]);
+
+    useDestroyAfter(id, duration);
     return (
       <div ref={ref}>
-        <h1>{background?.data}</h1>
+        <div
+          ref={modalRef}
+          className={styles.root}
+          style={{
+            transitionDuration: duration,
+          }}
+        >
+          <h1>{background?.data}</h1>
+        </div>
       </div>
     );
   },
