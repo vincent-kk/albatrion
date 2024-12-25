8ea71a7f7ce1c24ff81675a86b25829a1eec28d6
Vincent K. Kelvin <lunox273@gmail.com>
2024-12-25 18:34:10 +0900

    vitest error 수정

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,8 +1,8 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  "./packages/common-react/vite.config.ts",
-  "./packages/common/vite.config.ts",
+  "./packages/react-utils/vite.config.ts",
+  "./packages/common-utils/vite.config.ts",
   "./packages/schema-form-antd-plugin/vite.config.ts",
   "./packages/schema-form/vite.config.ts",
   "./packages/benchmark/vite.config.ts",
