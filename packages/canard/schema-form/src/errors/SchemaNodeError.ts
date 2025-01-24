75d5084be1150261a858ca536532d327aa6d3f96
Vincent K. Kelvin <lunox273@gmail.com>
2025-01-25 02:17:11 +0900

    canard의 error를 winglet error로 대체

--- packages/canard/schema-form/src/errors/SchemaNodeError.ts
+++ packages/canard/schema-form/src/errors/SchemaNodeError.ts
@@ -1,9 +1,9 @@
-import { BaseError, type ErrorDetails } from './BaseError';
+import { BaseError, type ErrorDetails } from '@winglet/common-utils';
 
 export class SchemaNodeError extends BaseError {
   static readonly #group = 'SCHEMA_NODE_ERROR';
   constructor(code: string, message: string, details: ErrorDetails = {}) {
-    super(`${SchemaNodeError.#group}.${code}`, message, details);
+    super(SchemaNodeError.#group, code, message, details);
     this.name = 'SchemaNodeError';
   }
 }
--- packages/canard/schema-form/src/errors/UnhandledErrors.ts
+++ packages/canard/schema-form/src/errors/UnhandledErrors.ts
@@ -1,9 +1,9 @@
-import { BaseError, type ErrorDetails } from './BaseError';
+import { BaseError, type ErrorDetails } from '@winglet/common-utils';
 
 export class UnhandledError extends BaseError {
   static readonly #group = 'UNHANDLED_ERROR';
   constructor(code: string, message: string, details: ErrorDetails = {}) {
-    super(`${UnhandledError.#group}.${code}`, message, details);
+    super(UnhandledError.#group, code, message, details);
     this.name = 'UnhandledError';
   }
 }
