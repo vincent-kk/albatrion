fc497a17551a601b9478217a1ecd4f9cee98426d
Vincent K. Kelvin <lunox273@gmail.com>
2025-01-27 00:14:58 +0900

    Add new hoc: withUploader

---
+++ packages/winglet/react-utils/src/hoc/withUploader/withUploader.tsx
@@ -0,0 +1,58 @@
+import {
+  type ChangeEvent,
+  type ComponentType,
+  Fragment,
+  memo,
+  useMemo,
+  useRef,
+} from 'react';
+
+import { isFunction } from '@winglet/common-utils';
+
+import type { Fn } from '@aileron/types';
+
+import { useHandle } from '../../hooks';
+
+interface BaseProps {
+  onClick?: Fn<[e?: MouseEvent]>;
+}
+
+interface UploaderProps {
+  acceptFormat?: string[];
+  onChange?: Fn<[file: File]>;
+}
+
+export const withUploader = <Props extends BaseProps>(
+  Component: ComponentType<Props>,
+) =>
+  memo(
+    ({ onClick, onChange, acceptFormat, ...props }: Props & UploaderProps) => {
+      const inputRef = useRef<HTMLInputElement>(null);
+      const accept = useMemo(() => acceptFormat?.join(','), [acceptFormat]);
+
+      const handleFileChange = useHandle(
+        ({ target }: ChangeEvent<HTMLInputElement>) => {
+          const file = target?.files?.[0];
+          if (file) onChange?.(file);
+        },
+      );
+
+      const handleClick = useHandle((e: MouseEvent) => {
+        if (isFunction(onClick)) onClick(e);
+        inputRef.current?.click();
+      });
+
+      return (
+        <Fragment>
+          <input
+            type="file"
+            accept={accept}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+            ref={inputRef}
+          />
+          <Component {...(props as Props)} onClick={handleClick} />
+        </Fragment>
+      );
+    },
+  );
