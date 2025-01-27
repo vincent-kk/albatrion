4aa76fc74fadd795095748ecf5ec072cc668d4ac
Vincent K. Kelvin <lunox273@gmail.com>
2025-01-28 01:52:28 +0900

    Preferred Frame 테스트용 스토리 추가

--- packages/lerx/promise-modal/coverage/NormalUsecase.stories.tsx
+++ packages/lerx/promise-modal/coverage/NormalUsecase.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { ModalProvider, alert, confirm, prompt } from '../src';
+import { Foreground } from './components/Foreground';
 
 export default {
   title: 'PromiseModal/NormalUsecase',
@@ -138,6 +139,95 @@ export const MultiModalUsecase = () => {
       <button onClick={handleAlert}>Open Alert</button>
       <button onClick={handleConfirm}>Open Confirm</button>
       <button onClick={handlePrompt}>Open Prompt: {value}</button>
+    </div>
+  );
+};
+
+export const PreferredFrameUsecase = () => {
+  const handleAlert = () => {
+    alert({
+      title: 'Hello, world!',
+      content: 'This is a test alert.',
+      closeOnBackdropClick: false,
+    }).then((result) => {
+      console.log(result);
+    });
+  };
+
+  const handleConfirm = () => {
+    confirm({
+      title: 'Hello, world!',
+      content: 'This is a test confirm.',
+      closeOnBackdropClick: false,
+    }).then((result) => {
+      console.log(result);
+    });
+  };
+
+  const [value, setValue] = useState('');
+
+  const handlePrompt = () => {
+    prompt({
+      title: 'Hello, world!',
+      content: 'This is a test prompt.',
+      Input: ({ defaultValue, onChange }) => {
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange(e.target.value);
+        };
+        return <input defaultValue={defaultValue} onChange={handleChange} />;
+      },
+      defaultValue: 'value',
+      closeOnBackdropClick: false,
+    }).then((value) => {
+      setValue(value);
+    });
+  };
+
+  const handleErrorPrompt = () => {
+    prompt({
+      title: 'Error Prompt',
+      content: 'Error will be thrown from Input',
+      Input: () => {
+        throw new Error('Error from Prompt Input');
+        return <div>Input</div>;
+      },
+    });
+  };
+
+  const handleAltForegroundAlert = () => {
+    alert({
+      title: 'Hello, world!',
+      content: 'This is a test alert, with alt foreground.',
+      closeOnBackdropClick: false,
+      ForegroundComponent: Foreground,
+      manualDestroy: true,
+    }).then((result) => {
+      console.log(result);
+    });
+  };
+
+  const handleAltBackgroundAlert = () => {
+    alert({
+      title: 'Hello, world!',
+      content: 'This is a test alert, with alt background.',
+      closeOnBackdropClick: false,
+    }).then((result) => {
+      console.log(result);
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleAlert}>Open Alert</button>
+      <button onClick={handleConfirm}>Open Confirm</button>
+      <button onClick={handlePrompt}>Open Prompt: {value}</button>
+      <button onClick={handleErrorPrompt}>Error from Prompt Input</button>
+      <button onClick={handleAltForegroundAlert}>
+        Open Alert with Alt Foreground
+      </button>
+      <button onClick={handleAltBackgroundAlert}>
+        Open Alert with Alt Background
+      </button>
     </div>
   );
 };
