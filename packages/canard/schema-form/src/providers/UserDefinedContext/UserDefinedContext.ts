86f58e73b09cb715649d7733a1779986663783bf
Vincent K. Kelvin <lunox273@gmail.com>
2025-03-02 21:19:07 +0900

    Form의 Context 타입 엄밀성 개선

--- packages/canard/schema-form/src/providers/UserDefinedContext/UserDefinedContext.ts
+++ packages/canard/schema-form/src/providers/UserDefinedContext/UserDefinedContext.ts
@@ -1,11 +1,14 @@
 import { createContext } from 'react';
 
+import { EMPTY_OBJECT } from '@winglet/common-utils';
+
 import type { Dictionary } from '@aileron/types';
 
 export interface UserDefinedContext {
+  /** 사용자 정의 컨텍스트 */
   context: Dictionary;
 }
 
-export const UserDefinedContext = createContext<UserDefinedContext>(
-  {} as UserDefinedContext,
-);
+export const UserDefinedContext = createContext<UserDefinedContext>({
+  context: EMPTY_OBJECT,
+});
--- packages/canard/schema-form/src/providers/UserDefinedContext/UserDefinedContextProvider.tsx
+++ packages/canard/schema-form/src/providers/UserDefinedContext/UserDefinedContextProvider.tsx
@@ -1,19 +1,24 @@
 import { PropsWithChildren, useMemo } from 'react';
 
+import { EMPTY_OBJECT } from '@winglet/common-utils';
+
 import type { FormProps } from '@/schema-form/components/Form';
 
 import { UserDefinedContext } from './UserDefinedContext';
 
 interface UserDefinedContextProviderProps {
   /** 사용자 정의 컨텍스트 */
-  context?: FormProps['context'];
+  context: FormProps['context'];
 }
 
 export const UserDefinedContextProvider = ({
   context,
   children,
 }: PropsWithChildren<UserDefinedContextProviderProps>) => {
-  const contextValue = useMemo(() => ({ context: context || {} }), [context]);
+  const contextValue = useMemo(
+    () => ({ context: context || EMPTY_OBJECT }),
+    [context],
+  );
   return (
     <UserDefinedContext.Provider value={contextValue}>
       {children}
