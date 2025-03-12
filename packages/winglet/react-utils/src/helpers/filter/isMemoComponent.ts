c703d7fa4f209045d8f60907ab74df8460c35fae
Vincent Kelvin <30566564+vincent-kk@users.noreply.github.com>
2025-03-13 01:06:38 +0900

    타입 추론 개선

--- packages/winglet/react-utils/src/helpers/filter/isMemoComponent.ts
+++ packages/winglet/react-utils/src/helpers/filter/isMemoComponent.ts
@@ -1,9 +1,14 @@
 import type { ComponentType, MemoExoticComponent } from 'react';
 
 // 메모이제이션된 컴포넌트 체크
-export const isMemoComponent = <Props extends object = any>(
+export const isMemoComponent = <
+  Props extends object = any,
+  Component extends MemoExoticComponent<
+    ComponentType<Props>
+  > = MemoExoticComponent<ComponentType<Props>>,
+>(
   component: unknown,
-): component is MemoExoticComponent<ComponentType<Props>> =>
+): component is Component =>
   typeof component === 'object' &&
   component !== null &&
   (component as any).$$typeof === Symbol.for('react.memo');
