790ef3066dedd71eb8f446096565d95de0762b66
Vincent K. Kelvin <lunox273@gmail.com>
2025-03-13 22:50:04 +0900

    기존 stringifyObject를 stringifyObjectSingleDepth로 변경

--- packages/winglet/common-utils/src/utils/object/index.ts
+++ packages/winglet/common-utils/src/utils/object/index.ts
@@ -1,4 +1,5 @@
 export { removeUndefined } from './removeUndefined';
 export { stringifyObject } from './stringifyObject';
+export { stringifyObjectSingleDepth } from './stringifyObjectSingleDepth';
 export { stringifyObjectWithFullSortedKeys } from './stringifyObjectWithFullSortedKeys';
 export { sortObjectKeys } from './sortObjectKeys';
