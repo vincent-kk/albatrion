164abda960b35f3bd753bceae6a1dc80df1c83e2
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-02 23:37:31 +0900

    [Refactor](promise-modal): change BootstrapProviderProps to type

--- packages/lerx/promise-modal/src/bootstrap/BootstrapProvider/type.ts
+++ packages/lerx/promise-modal/src/bootstrap/BootstrapProvider/type.ts
@@ -2,12 +2,11 @@ import type { Dictionary, Fn } from '@aileron/types';
 
 import type { ConfigurationContextProviderProps } from '@/promise-modal/providers/ConfigurationContext';
 
-export interface BootstrapProviderProps
-  extends ConfigurationContextProviderProps {
+export type BootstrapProviderProps = {
   usePathname?: Fn<[], { pathname: string }>;
   context?: Dictionary;
   root?: HTMLElement | null;
-}
+} & ConfigurationContextProviderProps;
 
 export interface BootstrapProviderHandle {
   bootstrap: Fn<[HTMLElement]>;
