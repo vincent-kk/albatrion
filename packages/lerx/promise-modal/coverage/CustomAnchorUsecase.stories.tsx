6f5bcc467c8a5377c8ac9fddbc3cd98132678c03
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-02 23:04:27 +0900

    [Feat](promise-modal): enhance custom anchor use case story

--- packages/lerx/promise-modal/coverage/CustomAnchorUsecase.stories.tsx
+++ packages/lerx/promise-modal/coverage/CustomAnchorUsecase.stories.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-import { ModalProvider, alert, confirm, prompt } from '../src';
-import { Foreground } from './components/Foreground';
+import {
+  ModalProvider,
+  ModalProviderHandle,
+  alert,
+  confirm,
+  prompt,
+} from '../src';
 
 export default {
   title: 'PromiseModal/CustomAnchorUsecase',
@@ -54,14 +59,30 @@ export const NormalUsecase = () => {
       },
     });
   };
+  const modalHandle = useRef<ModalProviderHandle>(null);
+  const modalRoot = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (modalRoot.current) {
+      modalHandle.current?.bootstrap(modalRoot.current);
+    }
+  }, []);
 
   return (
-    <div>
-      <button onClick={handleAlert}>Open Alert</button>
-      <button onClick={handleConfirm}>Open Confirm</button>
-      <button onClick={handlePrompt}>Open Prompt: {value}</button>
-      <button onClick={handleErrorPrompt}>Error from Prompt Input</button>
-    </div>
+    <ModalProvider ref={modalHandle}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+        <div
+          ref={modalRoot}
+          style={{ backgroundColor: 'red', width: '100%', height: 500 }}
+        />
+        <div>
+          <button onClick={handleAlert}>Open Alert</button>
+          <button onClick={handleConfirm}>Open Confirm</button>
+          <button onClick={handlePrompt}>Open Prompt: {value}</button>
+          <button onClick={handleErrorPrompt}>Error from Prompt Input</button>
+        </div>
+      </div>
+    </ModalProvider>
   );
 };
 
@@ -110,166 +131,6 @@ export const NestedModalProviderUsecase = () => {
       <ModalProvider SubtitleComponent={() => <div>Nested Modal</div>}>
         <button onClick={handleAlert}>Open Inner Provider Alert</button>
       </ModalProvider>
-    </div>
-  );
-};
-
-export const MultiModalUsecase = () => {
-  const handleAlert = () => {
-    alert({
-      title: 'Hello, world!',
-      content: 'This is a test alert.',
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  const handleConfirm = () => {
-    confirm({
-      title: 'Hello, world!',
-      content: 'This is a test confirm.',
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  const [value, setValue] = useState('');
-
-  const handlePrompt = () => {
-    prompt({
-      title: 'Hello, world!',
-      content: 'This is a test prompt.',
-      Input: ({ defaultValue, onChange }) => {
-        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-          onChange(e.target.value);
-        };
-        return <input defaultValue={defaultValue} onChange={handleChange} />;
-      },
-      defaultValue: 'value',
-    }).then((value) => {
-      setValue(value);
-    });
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      for (let i = 0; i < 10; i++) {
-        alert({
-          title: 'Hello, world!',
-          subtitle: `This is a test alert ${i}`,
-          content: 'This is a test alert.',
-        });
-      }
-      prompt({
-        title: 'Hello, world!',
-        content: 'This is a test prompt.',
-        Input: ({ defaultValue, onChange }) => {
-          const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            onChange(e.target.value);
-          };
-          return <input defaultValue={defaultValue} onChange={handleChange} />;
-        },
-        defaultValue: 'value',
-      }).then((value) => {
-        setValue(value);
-      });
-    }, 250);
-  }, []);
-
-  return (
-    <div>
-      <button onClick={handleAlert}>Open Alert</button>
-      <button onClick={handleConfirm}>Open Confirm</button>
-      <button onClick={handlePrompt}>Open Prompt: {value}</button>
-    </div>
-  );
-};
-
-export const PreferredFrameUsecase = () => {
-  const handleAlert = () => {
-    alert({
-      title: 'Hello, world!',
-      content: 'This is a test alert.',
-      closeOnBackdropClick: false,
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  const handleConfirm = () => {
-    confirm({
-      title: 'Hello, world!',
-      content: 'This is a test confirm.',
-      closeOnBackdropClick: false,
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  const [value, setValue] = useState('');
-
-  const handlePrompt = () => {
-    prompt({
-      title: 'Hello, world!',
-      content: 'This is a test prompt.',
-      Input: ({ defaultValue, onChange }) => {
-        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-          onChange(e.target.value);
-        };
-        return <input defaultValue={defaultValue} onChange={handleChange} />;
-      },
-      defaultValue: 'value',
-      closeOnBackdropClick: false,
-    }).then((value) => {
-      setValue(value);
-    });
-  };
-
-  const handleErrorPrompt = () => {
-    prompt({
-      title: 'Error Prompt',
-      content: 'Error will be thrown from Input',
-      Input: () => {
-        throw new Error('Error from Prompt Input');
-        return <div>Input</div>;
-      },
-    });
-  };
-
-  const handleAltForegroundAlert = () => {
-    alert({
-      title: 'Hello, world!',
-      content: 'This is a test alert, with alt foreground.',
-      closeOnBackdropClick: false,
-      ForegroundComponent: Foreground,
-      manualDestroy: true,
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  const handleAltBackgroundAlert = () => {
-    alert({
-      title: 'Hello, world!',
-      content: 'This is a test alert, with alt background.',
-      closeOnBackdropClick: false,
-    }).then((result) => {
-      console.log(result);
-    });
-  };
-
-  return (
-    <div>
-      <button onClick={handleAlert}>Open Alert</button>
-      <button onClick={handleConfirm}>Open Confirm</button>
-      <button onClick={handlePrompt}>Open Prompt: {value}</button>
-      <button onClick={handleErrorPrompt}>Error from Prompt Input</button>
-      <button onClick={handleAltForegroundAlert}>
-        Open Alert with Alt Foreground
-      </button>
-      <button onClick={handleAltBackgroundAlert}>
-        Open Alert with Alt Background
-      </button>
     </div>
   );
 };
