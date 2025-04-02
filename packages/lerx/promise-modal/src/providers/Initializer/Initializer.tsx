8dda47e8e3ba479fe654c129d4c12b4d259ca9af
Vincent Kelvin <30566564+vincent-kk@users.noreply.github.com>
2025-04-02 11:35:49 +0900

    [Refactor](promise-modal): simplify modal initialization logic

--- packages/lerx/promise-modal/src/providers/Initializer/Initializer.tsx
+++ packages/lerx/promise-modal/src/providers/Initializer/Initializer.tsx
@@ -1,22 +1,15 @@
 import { Fragment, type PropsWithChildren, useRef } from 'react';
 
-import { createPortal } from 'react-dom';
-
 import { printError } from '@winglet/common-utils';
 import { useOnMount, useTick } from '@winglet/react-utils';
 
 import type { Dictionary } from '@aileron/types';
 
 import { ModalManager } from '@/promise-modal/app/ModalManager';
-import { Anchor } from '@/promise-modal/components/Anchor';
 import { usePathname as useDefaultPathname } from '@/promise-modal/hooks/useDefaultPathname';
 
-import {
-  ConfigurationContextProvider,
-  type ConfigurationContextProviderProps,
-} from '../ConfigurationContext';
-import { ModalManagerContextProvider } from '../ModalManagerContext';
-import { UserDefinedContextProvider } from '../UserDefinedContext';
+import type { ConfigurationContextProviderProps } from '../ConfigurationContext';
+import { initializeModal } from './initializeModal';
 
 interface InitializerProps extends ConfigurationContextProviderProps {
   context?: Dictionary;
@@ -65,24 +58,18 @@ export const Initializer = ({
     <Fragment>
       {children}
       {portalRef.current &&
-        createPortal(
-          <UserDefinedContextProvider context={context}>
-            <ConfigurationContextProvider
-              ForegroundComponent={ForegroundComponent}
-              BackgroundComponent={BackgroundComponent}
-              TitleComponent={TitleComponent}
-              SubtitleComponent={SubtitleComponent}
-              ContentComponent={ContentComponent}
-              FooterComponent={FooterComponent}
-              options={options}
-            >
-              <ModalManagerContextProvider pathname={pathname}>
-                <Anchor />
-              </ModalManagerContextProvider>
-            </ConfigurationContextProvider>
-          </UserDefinedContextProvider>,
-          portalRef.current,
-        )}
+        initializeModal({
+          ForegroundComponent,
+          BackgroundComponent,
+          TitleComponent,
+          SubtitleComponent,
+          ContentComponent,
+          FooterComponent,
+          options,
+          context,
+          pathname,
+          root: portalRef.current,
+        })}
     </Fragment>
   );
 };
