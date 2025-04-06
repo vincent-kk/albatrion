48f9843f425af3b02271370dea36789c808e045d
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-07 01:25:57 +0900

    [Fix](common-utils): update deepClone to use WeakMap for cache

--- packages/winglet/common-utils/src/utils/object/deepClone.ts
+++ packages/winglet/common-utils/src/utils/object/deepClone.ts
@@ -13,14 +13,14 @@ import { getSymbols } from './getSymbols';
 
 export const deepClone = <Type>(target: Type): Type => clone(target);
 
-const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
+const clone = <Type>(value: Type, cache = new WeakMap<object, any>()): Type => {
   if (isPrimitiveType(value)) return value as Type;
 
-  if (cache.has(value)) return cache.get(value) as Type;
+  if (cache.has(value as object)) return cache.get(value as object) as Type;
 
   if (isArray(value)) {
     const result: any = new Array(value.length);
-    cache.set(value, result);
+    cache.set(value as object, result);
     for (let i = 0; i < value.length; i++) result[i] = clone(value[i], cache);
     if ('index' in value) result.index = value.index;
     if ('input' in value) result.input = value.input;
@@ -37,14 +37,14 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
 
   if (value instanceof Map) {
     const result = new Map();
-    cache.set(value, result);
+    cache.set(value as object, result);
     for (const [k, v] of value) result.set(k, clone(v, cache));
     return result as Type;
   }
 
   if (value instanceof Set) {
     const result = new Set();
-    cache.set(value, result);
+    cache.set(value as object, result);
     for (const v of value) result.add(clone(v, cache));
     return result as Type;
   }
@@ -53,7 +53,7 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
 
   if (isTypedArray(value)) {
     const result = new (Object.getPrototypeOf(value).constructor)(value.length);
-    cache.set(value, result);
+    cache.set(value as object, result);
     for (let i = 0; i < value.length; i++) result[i] = clone(value[i], cache);
     return result as Type;
   }
@@ -68,7 +68,7 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
       value.byteOffset,
       value.byteLength,
     );
-    cache.set(value, result);
+    cache.set(value as object, result);
     copyProperties(result, value, cache);
     return result as Type;
   }
@@ -77,21 +77,21 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
     const result = new File([value], value.name, {
       type: value.type,
     });
-    cache.set(value, result);
+    cache.set(value as object, result);
     copyProperties(result, value, cache);
     return result as Type;
   }
 
   if (isBlob(value)) {
     const result = new Blob([value], { type: value.type });
-    cache.set(value, result);
+    cache.set(value as object, result);
     copyProperties(result, value, cache);
     return result as Type;
   }
 
   if (value instanceof Error) {
     const result = new (value.constructor as { new (): Error })();
-    cache.set(value, result);
+    cache.set(value as object, result);
     result.message = value.message;
     result.name = value.name;
     result.stack = value.stack;
@@ -104,7 +104,7 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
 
   if (typeof value === 'object' && isCloneable(value)) {
     const result = Object.create(Object.getPrototypeOf(value));
-    cache.set(value, result);
+    cache.set(value as object, result);
     // isCloneable will not allow to pass nullish object
     copyProperties(result, value!, cache);
     return result as Type;
@@ -116,14 +116,22 @@ const clone = <Type>(value: Type, cache = new Map<any, any>()): Type => {
 const copyProperties = (
   target: Record<PropertyKey, any>,
   source: Record<PropertyKey, any>,
-  cache?: Map<any, any>,
+  cache?: WeakMap<object, any>,
 ): void => {
-  const keys = [...Object.keys(source), ...getSymbols(source)];
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const descriptor = Object.getOwnPropertyDescriptor(target, key);
-    if (descriptor == null || descriptor.writable) {
-      target[key] = clone(source[key], cache);
+  const keys = Object.keys(source);
+  if (keys.length > 0)
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const descriptor = Object.getOwnPropertyDescriptor(target, key);
+      if (descriptor == null || descriptor.writable)
+        target[key] = clone(source[key], cache);
     }
-  }
+  const symbols = getSymbols(source);
+  if (symbols.length > 0)
+    for (let i = 0; i < symbols.length; i++) {
+      const key = symbols[i];
+      const descriptor = Object.getOwnPropertyDescriptor(target, key);
+      if (descriptor == null || descriptor.writable)
+        target[key] = clone(source[key], cache);
+    }
 };
