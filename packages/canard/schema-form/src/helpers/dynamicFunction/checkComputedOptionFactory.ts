7817fecbd05e391e21d53b1e355f13166b24e397
Vincent Kelvin <30566564+vincent-kk@users.noreply.github.com>
2025-04-09 19:08:33 +0900

    [Refactor](schema-form): improve computed option handling

--- packages/canard/schema-form/src/helpers/dynamicFunction/checkComputedOptionFactory.ts
+++ packages/canard/schema-form/src/helpers/dynamicFunction/checkComputedOptionFactory.ts
@@ -1,3 +1,4 @@
+import { isString } from '@winglet/common-utils';
 import { JSONPath } from '@winglet/json-schema';
 
 import type { Fn } from '@aileron/types';
@@ -9,19 +10,27 @@ const JSON_PATH_REGEX = new RegExp(
 
 type CheckComputedOption = Fn<[dependencies: any[]], boolean>;
 
+/**
+ * jsonPath 표현식을 확인하는 함수를 생성합니다.
+ * @param dependencyPaths - 의존성 경로 배열
+ * @param expression - 평가할 표현식
+ * @returns 의존성 배열을 매개변수로 받는 함수 또는 undefined
+ */
 export const checkComputedOptionFactory = (
   dependencyPaths: string[],
   expression: string | boolean | undefined,
 ): CheckComputedOption | undefined => {
-  if (typeof expression !== 'string') return;
-  const functionBody = `return !!(${expression
+  if (!isString(expression)) return;
+  const computedExpression = expression
     .replace(JSON_PATH_REGEX, (path) => {
-      if (!dependencyPaths.includes(path)) {
-        dependencyPaths.push(path);
-      }
+      if (!dependencyPaths.includes(path)) dependencyPaths.push(path);
       return `dependencies[${dependencyPaths.indexOf(path)}]`;
     })
     .trim()
-    .replace(/;$/, '')})`;
-  return new Function('dependencies', functionBody) as CheckComputedOption;
+    .replace(/;$/, '');
+  if (computedExpression.length === 0) return;
+  return new Function(
+    'dependencies',
+    `return !!(${computedExpression})`,
+  ) as CheckComputedOption;
 };
--- packages/canard/schema-form/src/helpers/dynamicFunction/getWatchValuesFactory.ts
+++ packages/canard/schema-form/src/helpers/dynamicFunction/getWatchValuesFactory.ts
@@ -4,15 +4,31 @@ import type { Fn } from '@aileron/types';
 
 type GetWatchValues = Fn<[dependencies: any[]], any[]>;
 
+/**
+ * 의존성 경로와 watchValue paths를 받아 watchValues를 반환하는 함수를 생성합니다.
+ * @param dependencyPaths - 의존성 경로 배열
+ * @param watch - watchValue paths
+ * @returns watchValue를 반환하는 함수 또는 undefined
+ */
 export const getWatchValuesFactory = (
   dependencyPaths: string[],
   watch: string | string[] | undefined,
 ): GetWatchValues | undefined => {
   if (!watch || !(isString(watch) || isArray(watch))) return;
-  const watchValueIndexes = (isArray(watch) ? watch : [watch]).map((path) => {
+  const watchValues = isArray(watch) ? watch : [watch];
+  const watchValueIndexes = [] as number[];
+  for (let i = 0; i < watchValues.length; i++) {
+    const path = watchValues[i];
     if (!dependencyPaths.includes(path)) dependencyPaths.push(path);
-    return dependencyPaths.indexOf(path);
-  });
-  const functionBody = `return [${watchValueIndexes.join(',')}].map((index) => dependencies[index])`;
-  return new Function('dependencies', functionBody) as GetWatchValues;
+    watchValueIndexes.push(dependencyPaths.indexOf(path));
+  }
+  if (watchValueIndexes.length === 0) return;
+  return new Function(
+    'dependencies',
+    `const result = [];
+     const indexes = [${watchValueIndexes.join(',')}];
+     for (let i = 0; i < indexes.length; i++)
+       result.push(dependencies[indexes[i]]);
+     return result;`,
+  ) as GetWatchValues;
 };
