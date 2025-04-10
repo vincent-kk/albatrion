6e7a33c0c7422ac4eb34d891276129a50b6e78df
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-11 01:34:31 +0900

    [Refactor](common-utils): optimize unit constants for clarity

--- packages/winglet/common-utils/src/constant/unit.ts
+++ packages/winglet/common-utils/src/constant/unit.ts
@@ -1,6 +1,13 @@
-export const KILO = 1000;
-export const MEGA = 1000 * KILO;
-export const GIGA = 1000 * MEGA;
-export const TERA = 1000 * GIGA;
-export const PETA = 1000 * TERA;
-export const EXA = 1000 * PETA;
+export const KILO = 10 ** 3;
+export const MEGA = KILO * KILO;
+export const GIGA = MEGA * KILO;
+export const TERA = GIGA * KILO;
+export const PETA = TERA * KILO;
+export const EXA = PETA * KILO;
+
+export const KILO_2 = 2 ** 10;
+export const MEGA_2 = KILO_2 * KILO_2;
+export const GIGA_2 = MEGA_2 * KILO_2;
+export const TERA_2 = GIGA_2 * KILO_2;
+export const PETA_2 = TERA_2 * KILO_2;
+export const EXA_2 = PETA_2 * KILO_2;
