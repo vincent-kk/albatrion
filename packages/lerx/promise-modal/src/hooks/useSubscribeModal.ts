aaf98ae60d76e37a2abd5f1adc4ea773e491eca0
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-14 01:21:26 +0900

    [Refactor](promise-modal): replace useOnMount with useEffect

--- packages/lerx/promise-modal/src/hooks/useSubscribeModal.ts
+++ packages/lerx/promise-modal/src/hooks/useSubscribeModal.ts
@@ -1,13 +1,15 @@
-import { useOnMount, useVersion } from '@winglet/react-utils';
+import { useEffect } from 'react';
+
+import { useVersion } from '@winglet/react-utils';
 
 import type { ModalNode } from '@/promise-modal/core';
 
 export const useSubscribeModal = (modal?: ModalNode) => {
   const [version, update] = useVersion();
-  useOnMount(() => {
+  useEffect(() => {
     if (!modal) return;
     const unsubscribe = modal.subscribe(update);
     return unsubscribe;
-  });
+  }, [modal]);
   return version;
 };
