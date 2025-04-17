33f42d18afe181ba6ee9e3a30c2b4bcefeb78eff
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-18 01:50:20 +0900

    [Refactor](schema-form): update event types to include computed properties

--- packages/canard/schema-form/src/core/tests/BooleanNode.test.ts
+++ packages/canard/schema-form/src/core/tests/BooleanNode.test.ts
@@ -91,7 +91,10 @@ describe('BooleanNode', () => {
 
     // 이벤트가 발생했는지 확인
     expect(mockListener).toHaveBeenCalledWith({
-      type: NodeEventType.UpdateValue | NodeEventType.Refresh,
+      type:
+        NodeEventType.UpdateValue |
+        NodeEventType.Refresh |
+        NodeEventType.UpdateComputedProperties,
       payload: {
         [NodeEventType.UpdateValue]: true,
       },
