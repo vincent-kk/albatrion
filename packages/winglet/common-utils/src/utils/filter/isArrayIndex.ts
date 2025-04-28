b6364078570ad349ada35b0c4b7f3dad4a3ce309
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-28 19:22:50 +0900

    [Test](common-utils): enhance isArrayIndex tests for validation

--- packages/winglet/common-utils/src/utils/filter/isArrayIndex.ts
+++ packages/winglet/common-utils/src/utils/filter/isArrayIndex.ts
@@ -1,5 +1,12 @@
-export const isArrayIndex = (key: string): boolean => {
-  if (key === '') return false;
-  const index = +key;
-  return index >>> 0 === index && index < 0xffffffff;
+export const isArrayIndex = (value: string): boolean => {
+  if (!value) return false;
+  let character;
+  let index = 0;
+  const length = value.length;
+  while (index < length) {
+    character = value.charCodeAt(index);
+    if (character < 48 || character > 57) return false;
+    index++;
+  }
+  return true;
 };
