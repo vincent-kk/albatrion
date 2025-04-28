969d4477d416bdf329295087b323802cbd07aaf9
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-29 00:59:10 +0900

    [Refactor](common-utils): rename omitKeys to emits in utility functions

--- packages/winglet/common-utils/src/utils/object/getObjectKeys.ts
+++ packages/winglet/common-utils/src/utils/object/getObjectKeys.ts
@@ -2,16 +2,16 @@ import type { Dictionary, Fn } from '@aileron/declare';
 
 export const getObjectKeys = <Type extends Dictionary>(
   object: Type,
-  omitKeys?: Array<keyof Type>,
+  emits?: Array<keyof Type>,
   sort?: Fn<[a: string, b: string], number>,
 ): Array<keyof Type> => {
-  const omit = omitKeys ? new Set(omitKeys) : null;
+  const emitSet = emits ? new Set(emits) : null;
   const sortedKeys = Object.keys(object).sort(sort);
-  if (!omit) return sortedKeys as Array<keyof Type>;
+  if (!emitSet) return sortedKeys as Array<keyof Type>;
   const keys: Array<keyof Type> = [];
   for (let i = 0; i < sortedKeys.length; i++) {
     const key = sortedKeys[i];
-    if (!omit.has(key)) keys.push(key);
+    if (!emitSet.has(key)) keys.push(key);
   }
   return keys;
 };
