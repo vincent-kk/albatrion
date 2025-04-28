cffc756014f497796033ea5a280f513cc0135346
Vincent K. Kelvin <lunox273@gmail.com>
2025-04-28 19:22:44 +0900

    [Feat](common-utils): add compilePointer utility function

--- packages/winglet/common-utils/src/utils/json/JSONPointer/index.ts
+++ packages/winglet/common-utils/src/utils/json/JSONPointer/index.ts
@@ -1,3 +1,4 @@
 export { JSONPointer } from './enum';
 export { getValueByPointer } from './getValueByPointer';
 export { setValueByPointer } from './setValueByPointer';
+export { compilePointer } from './utils/compileSegments';
