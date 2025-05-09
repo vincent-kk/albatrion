5b25d1fb916858cca152d8f4a4899aa9dec66925
Vincent K. Kelvin <lunox273@gmail.com>
2025-05-10 02:19:17 +0900

    [Docs](common-utils): add JSDoc comments for error classes and utilities

--- packages/winglet/common-utils/src/errors/TimeoutError.ts
+++ packages/winglet/common-utils/src/errors/TimeoutError.ts
@@ -1,12 +1,27 @@
 import { BaseError, type ErrorDetails } from './BaseError';
 
+/**
+ * 작업이 지정된 시간 내에 완료되지 않았을 때 발생하는 오류
+ * 시간 제한이 있는 비동기 작업에서 사용됨
+ */
 export class TimeoutError extends BaseError {
   static readonly #group = 'TIMEOUT';
+  /**
+   * TimeoutError 생성자
+   * @param code - 구체적인 오류 코드
+   * @param message - 오류 메시지
+   * @param details - 오류 관련 추가 정보
+   */
   constructor(code: string, message: string, details: ErrorDetails = {}) {
     super(TimeoutError.#group, code, message, details);
     this.name = 'Timeout';
   }
 }
 
+/**
+ * 주어진 오류가 TimeoutError 인지 확인하는 타입 가드 함수
+ * @param error - 확인할 오류 객체
+ * @returns error가 TimeoutError 인지 여부
+ */
 export const isTimeoutError = (error: unknown): error is TimeoutError =>
   error instanceof TimeoutError;
--- packages/winglet/common-utils/src/libs/getKeys.ts
+++ packages/winglet/common-utils/src/libs/getKeys.ts
@@ -1,6 +1,14 @@
 import { hasOwnProperty } from './hasOwnProperty';
 
+/**
+ * 객체 또는 배열의 키(인덱스 또는 프로퍼티 이름)를 배열로 반환하는 함수
+ * 배열, 객체 및 기타 값 유형에 대해 동작
+ * @template Value - 키를 추출할 값의 타입
+ * @param value - 키를 추출할 값
+ * @returns 값에서 추출한 키들의 배열
+ */
 export const getKeys = <Value>(value: Value) => {
+  // 배열인 경우 인덱스를 문자열로 반환
   if (Array.isArray(value)) {
     const keys = new Array(value.length);
     for (let k = 0; k < keys.length; k++) keys[k] = '' + k;
