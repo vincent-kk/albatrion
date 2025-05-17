1e7f769304a2ce56d9aaba59915dffe3f0bcf011
Vincent K. Kelvin <lunox273@gmail.com>
2025-05-17 16:17:40 +0900

    [Feat](schema-form): add parseObject function for object parsing

--- packages/canard/schema-form/src/core/parsers/index.ts
+++ packages/canard/schema-form/src/core/parsers/index.ts
@@ -1,4 +1,5 @@
 export { parseArray } from './parseArray';
 export { parseBoolean } from './parseBoolean';
 export { parseNumber } from './parseNumber';
+export { parseObject } from './parseObject';
 export { parseString } from './parseString';
--- packages/canard/schema-form/src/core/parsers/parseObject.ts
+++ packages/canard/schema-form/src/core/parsers/parseObject.ts
@@ -1,6 +1,6 @@
-import { isArray } from '@winglet/common-utils';
+import { isPlainObject } from '@winglet/common-utils';
 
-import type { ArrayValue } from '@/schema-form/types';
+import type { ObjectValue } from '@/schema-form/types';
 
 /**
  * 입력값을 배열 형식으로 분석합니다.
@@ -8,5 +8,5 @@ import type { ArrayValue } from '@/schema-form/types';
  * @returns 분석된 배열 또는 배열이 아닌 경우 빈 배열
  * @typeParam T - 배열 요소의 타입
  */
-export const parseArray = (value: unknown): ArrayValue =>
-  isArray(value) ? value : [];
+export const parseObject = (value: unknown): ObjectValue =>
+  isPlainObject(value) ? value : {};
