afe753a03fa416533fa7a5e720ff0053d04e111a
Vincent K. Kelvin <lunox273@gmail.com>
2025-05-26 00:08:26 +0900

    [Docs](utility): update comments for clarity and accuracy

--- packages/aileron/common/declare/utility.d.ts
+++ packages/aileron/common/declare/utility.d.ts
@@ -1,25 +1,25 @@
 export type Nullish<T> = T | null | undefined;
 
-/** T에서 key in K를 추출하고, 이를 Required로 설정 */
+/** Extract keys K from T and make them required */
 export type PickRequired<T, K extends keyof T> = Required<Pick<T, K>>;
 
-/** T에서 key in K를 추출하고, 이를 Partial로 설정 */
+/** Extract keys K from T and make them partial */
 export type PickPartial<T, K extends keyof T> = Partial<Pick<T, K>>;
 
-/** T에서 key in K를 제거하고, 나머지를 Required로 설정 */
+/** Omit keys K from T and make the rest required */
 export type OmitRequired<T, K extends keyof T> = Required<Omit<T, K>>;
 
-/** T에서 key in K를 제거하고, 나머지를 Partial로 설정 */
+/** Omit keys K from T and make the rest partial */
 export type OmitPartial<T, K extends keyof T> = Partial<Omit<T, K>>;
 
-/** T에서 key in K는 Required로 설정하고, 나머지는 Partial로 설정 */
+/** Make keys K required and the rest partial in T */
 export type PickAndPartial<T, K extends keyof T> = PickRequired<T, K> &
   OmitPartial<T, K>;
 
-/** T에서 K를 Required로 설정하고, 나머지는 유지 */
+/** Make keys K required in T while keeping the rest unchanged */
 export type RequiredBy<T, K extends keyof T> = PickRequired<T, K> & T;
 
-/** T에서 K를 Partial로 설정하고, 나머지는 유지 */
+/** Make keys K partial in T while keeping the rest unchanged */
 export type PartialBy<T, K extends keyof T> = PickPartial<T, K> & Omit<T, K>;
 
 export type Roll<T> = { [K in keyof T]: T[K] };
