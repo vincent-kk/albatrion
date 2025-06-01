b4c9a86c9e638518416d130f53b23da88d734c59
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-01 16:43:32 +0900

    [Docs](common-utils): update JSONPath and JSONPointer enums with detailed comments

--- packages/winglet/common-utils/src/utils/json/JSONPath/enum.ts
+++ packages/winglet/common-utils/src/utils/json/JSONPath/enum.ts
@@ -1,15 +1,32 @@
 /**
  * Special character constants used in JSONPath expressions
+ * @see https://goessner.net/articles/JsonPath
  */
 export enum JSONPath {
-  /** Root node of the data ($) */
+  /**
+   * Root node of the data (`$`)
+   * @see https://goessner.net/articles/JsonPath/index.html#e2
+   */
   Root = '$',
-  /** Parent node of the current node (_), this is not a official JSONPath syntax */
+  /**
+   * Parent node of the current node (`_`)
+   * @note This is not a official JSONPath syntax, but it is used in some implementations.
+   * @see https://goessner.net/articles/JsonPath/index.html#e2
+   */
   Parent = '_',
-  /** Currently processing node (@) */
+  /**
+   * Currently processing node (`@`)
+   * @see https://goessner.net/articles/JsonPath/index.html#e2
+   */
   Current = '@',
-  /** Child node access operator (.) */
+  /**
+   * Child node access operator (`.`)
+   * @see https://goessner.net/articles/JsonPath/index.html#e2
+   */
   Child = '.',
-  /** Filter condition operator (#) */
+  /**
+   * Filter condition operator (`#`)
+   * @see https://goessner.net/articles/JsonPath/index.html#e2
+   */
   Filter = '#',
 }
--- packages/winglet/common-utils/src/utils/json/JSONPointer/enum.ts
+++ packages/winglet/common-utils/src/utils/json/JSONPointer/enum.ts
@@ -1,10 +1,19 @@
 /**
  * Special character constants used in JSONPointer expressions
  * Based on RFC 6901 specification
+ * @see https://datatracker.ietf.org/doc/html/rfc6901
  */
 export enum JSONPointer {
-  /** Starting character of URI fragment identifier (#) */
+  /**
+   * Starting character of URI fragment identifier (`#`)
+   * @note `#` or `''` can be used as a root pointer.
+   * @notice if you not want to use `#` as a root pointer, just start with `/`.
+   * @see https://datatracker.ietf.org/doc/html/rfc6901#section-6
+   */
   Root = '#',
-  /** Path separator character (/) */
+  /**
+   * Path separator character (`/`)
+   * @see https://datatracker.ietf.org/doc/html/rfc6901#section-3
+   */
   Child = '/',
 }
