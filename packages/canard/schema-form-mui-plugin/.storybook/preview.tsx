a520e65b863a9b7b3c1b808a142bc6924e2ed24c
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-02 22:35:01 +0900

    [Chore](schema-form-mui-plugin): initialize project structure with config files

---
+++ packages/canard/schema-form-mui-plugin/.storybook/preview.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import type { Preview } from '@storybook/react';
+import { ConfigProvider } from 'antd';
+
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+} from '../../../aileron/constants/storybook';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+        order: ['Page', 'Components', '*'],
+      },
+    },
+  },
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: Theme.LIGHT,
+      toolbar: {
+        icon: 'circlehollow',
+        items: [Theme.LIGHT, Theme.DARK],
+        showName: true,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  (Story, context) => {
+    const theme = context.globals.theme;
+    const prevTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY);
+    if (prevTheme !== theme) {
+      localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+      document.documentElement.setAttribute('data-theme', theme);
+      window.top?.location.reload();
+    }
+    return <Story />;
+  },
+  (Story) => (
+    <ConfigProvider theme={{ token: { colorError: '#ff4d4f' } }}>
+      <Story />
+    </ConfigProvider>
+  ),
+];
+
+export default preview;
