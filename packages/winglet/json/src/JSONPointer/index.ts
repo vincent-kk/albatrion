9cf47ba647f65f1061ce95f44b6fcf24ae6397ae
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-08 16:18:25 +0900

    [Refactor](winglet): simplify exports in JSONPointer module

--- packages/winglet/json/src/JSONPointer/index.ts
+++ packages/winglet/json/src/JSONPointer/index.ts
@@ -1,21 +1,5 @@
 export { JSONPointer } from './enum';
 
-export { escapePointer } from './utils/escape/escapePointer';
-export { unescapePointer } from './utils/escape/unescapePointer';
-export { getValueByPointer } from './utils/manipulator/getValueByPointer';
-export { setValueByPointer } from './utils/manipulator/setValueByPointer';
-
-export { applyPatch, type ApplyPatchOptions } from './utils/patch/applyPatch';
-export { compare, type CompareOptions } from './utils/patch/compare';
-export { difference } from './utils/patch/difference';
-export { mergePatch } from './utils/patch/mergePatch';
-
-export type {
-  Patch,
-  TestPatch,
-  AddPatch,
-  ReplacePatch,
-  RemovePatch,
-  CopyPatch,
-  MovePatch,
-} from './utils/patch/type';
+export * from './utils/escape';
+export * from './utils/manipulator';
+export * from './utils/patch';
--- packages/winglet/json/src/index.ts
+++ packages/winglet/json/src/index.ts
@@ -1,4 +1,7 @@
 export { JSONPath } from './JSONPath';
 export { JSONPointer } from './JSONPointer';
 
-export * from './JSONPointer/utils';
+export * from './JSONPointer/utils/escape';
+export * from './JSONPointer/utils/getJSONPointer';
+export * from './JSONPointer/utils/manipulator';
+export * from './JSONPointer/utils/patch';
