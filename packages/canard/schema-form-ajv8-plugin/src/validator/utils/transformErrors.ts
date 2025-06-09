ff716d1d42f78444246a96432f290527f0364708
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-10 04:05:19 +0900

    [Refactor](schema-form-ajv8-plugin): update dataPath to use JSON Pointer

--- packages/canard/schema-form-ajv8-plugin/src/validator/utils/transformErrors.ts
+++ packages/canard/schema-form-ajv8-plugin/src/validator/utils/transformErrors.ts
@@ -1,8 +1,6 @@
 import type { JsonSchemaError } from '@canard/schema-form';
-import { isArrayIndex } from '@winglet/common-utils/filter';
 import type { ErrorObject } from 'ajv';
 
-const JSON_PATH_SEPARATOR = '.';
 const JSON_POINTER_SEPARATOR = '/';
 
 export const transformErrors = (errors: ErrorObject[]): JsonSchemaError[] => {
@@ -25,31 +23,11 @@ const transformDataPath = (error: ErrorObject): string => {
   const instancePath = error.instancePath;
   const hasMissingProperty =
     error.keyword === 'required' && error.params?.missingProperty;
-
   if (!instancePath)
     return hasMissingProperty
-      ? JSON_PATH_SEPARATOR + error.params.missingProperty
+      ? JSON_POINTER_SEPARATOR + error.params.missingProperty
       : '';
-
-  const parts = [];
-  let segmentStart = 1;
-
-  for (let i = 1; i <= instancePath.length; i++) {
-    if (
-      i === instancePath.length ||
-      instancePath[i] === JSON_POINTER_SEPARATOR
-    ) {
-      if (segmentStart < i) {
-        const segment = instancePath.slice(segmentStart, i);
-        if (isArrayIndex(segment)) parts.push('[' + segment + ']');
-        else parts.push(JSON_PATH_SEPARATOR + segment);
-      }
-      segmentStart = i + 1;
-    }
-  }
-
-  const result = parts.join('');
   return hasMissingProperty
-    ? result + JSON_PATH_SEPARATOR + error.params.missingProperty
-    : result;
+    ? instancePath + JSON_POINTER_SEPARATOR + error.params.missingProperty
+    : instancePath;
 };
