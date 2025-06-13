8b0bdee1bd893b2a84feef527211576e3623f6aa
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-13 20:12:39 +0900

    [Build](data-loader): update rollup config for ESM and CJS bundles

--- packages/winglet/data-loader/rollup.config.mjs
+++ packages/winglet/data-loader/rollup.config.mjs
@@ -1,31 +1,34 @@
 // 번들 빌드 예제 - rollup.config.bundle.mjs
-import {
-  getBundleBuildOptions,
-  getEntrypoints,
-} from '../../aileron/script/build/rollup.bundle.mjs';
+import { createRequire } from 'node:module';
 
-const packageJson = createRequire(import.meta.url)('./package.json');
+import { getBundleBuildOptions } from '../../aileron/script/build/rollup.bundle.mjs';
 
 const { bundleBuildOptions, clearDir } = getBundleBuildOptions(import.meta.url);
 
-export default () => {
+const packageJson = createRequire(import.meta.url)('./package.json');
+
+const mainEntry = packageJson.exports['.'];
+
+export default async () => {
   clearDir('dist');
-  const entrypoints = getEntrypoints(packageJson);
+
   return [
-    bundleBuildOptions({
-      entrypoints,
+    // ESM 단일 번들
+    await bundleBuildOptions({
+      entry: mainEntry.source,
       format: 'esm',
-      outDir: 'dist',
+      outFile: mainEntry.import,
       sourcemap: true,
-      minify: true,
+      minify: false,
       optimizeImports: true,
     }),
-    bundleBuildOptions({
-      entrypoints,
+    // CJS 단일 번들
+    await bundleBuildOptions({
+      entry: mainEntry.source,
       format: 'cjs',
-      outDir: 'dist',
+      outFile: mainEntry.require,
       sourcemap: true,
-      minify: true,
+      minify: false,
       optimizeImports: true,
     }),
   ];
