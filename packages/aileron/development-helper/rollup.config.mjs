661c6d4d03e403ae9e190738232f8e3f6285607d
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-14 02:04:00 +0900

    [Refactor](development-helper): simplify rollup config and add exports

---
+++ packages/aileron/development-helper/rollup.config.mjs
@@ -0,0 +1,44 @@
+import { createRequire } from 'node:module';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import {
+  getEntrypoints,
+  getLibBuildOptions,
+} from '../../aileron/script/build/rollup.transpile.mjs';
+
+const packageJson = createRequire(import.meta.url)('./package.json');
+
+const { libBuildOptions, clearDir } = getLibBuildOptions(import.meta.url);
+
+export default () => {
+  clearDir('dist');
+  const entrypoints = getEntrypoints(packageJson);
+  return [
+    libBuildOptions({
+      format: 'esm',
+      extension: 'mjs',
+      entrypoints,
+      outDir: 'dist',
+      sourcemap: false,
+      tsconfigCompilerOptions: {
+        importHelpers: false,
+      },
+      plugins: {
+        beforeTransform: [peerDepsExternal()],
+      },
+    }),
+    libBuildOptions({
+      format: 'cjs',
+      extension: 'cjs',
+      entrypoints,
+      outDir: 'dist',
+      sourcemap: false,
+      tsconfigCompilerOptions: {
+        importHelpers: false,
+      },
+      plugins: {
+        beforeTransform: [peerDepsExternal()],
+      },
+    }),
+  ];
+};
