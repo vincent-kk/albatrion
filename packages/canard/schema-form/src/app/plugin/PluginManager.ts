5c7b0dbaa7a0c39edbd8f17d282855cac8ac4aba
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-13 20:30:25 +0900

    [Build](promise-modal): update imports and add TypeScript config

--- packages/canard/schema-form/src/app/plugin/PluginManager.ts
+++ packages/canard/schema-form/src/app/plugin/PluginManager.ts
@@ -20,7 +20,7 @@ import type {
   FormatError,
 } from '@/schema-form/types';
 
-import { ValidatorPlugin } from './type';
+import type { ValidatorPlugin } from './type';
 
 interface RenderKit {
   FormGroup: ComponentType<FormTypeRendererProps>;
