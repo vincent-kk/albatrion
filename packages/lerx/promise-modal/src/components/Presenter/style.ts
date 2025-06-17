bf37b4bcdcc077b8866944cf0f7cac0f9126d75d
Vincent K. Kelvin <lunox273@gmail.com>
2025-06-17 23:56:20 +0900

    [Refactor](promise-modal): update Presenter to use hashed class names

--- packages/lerx/promise-modal/src/components/Presenter/style.ts
+++ packages/lerx/promise-modal/src/components/Presenter/style.ts
@@ -1,7 +1,13 @@
-export const style = `
-[data-presenter] {
+import { ModalManager } from '@/promise-modal/app/ModalManager';
+
+export const presenter = ModalManager.getHashedClassNames('presenter');
+
+const style = `
+.${presenter} {
   position: fixed;
   inset: 0;
   pointer-events: none;
   overflow: hidden;
 `;
+
+ModalManager.defineStyleSheet('presenter', style);
