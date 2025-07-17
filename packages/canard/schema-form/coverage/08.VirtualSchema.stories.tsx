3ea1fcaf8640ce14b026917142873aa9fa31104b
Vincent K. Kelvin <lunox273@gmail.com>
2025-07-18 03:16:06 +0900

    [Feature](VirtualSchema): add new story for virtual node required usecase

--- packages/canard/schema-form/coverage/08.VirtualSchema.stories.tsx
+++ packages/canard/schema-form/coverage/08.VirtualSchema.stories.tsx
@@ -49,6 +49,64 @@ export const VirtualSchema = () => {
   );
 };
 
+export const VirtualSchemaRequired = () => {
+  const jsonSchema = {
+    type: 'object',
+    properties: {
+      control: {
+        type: 'string',
+        enum: ['A', 'B', 'C'],
+        default: 'A',
+      },
+      alwaysVisible: {
+        type: 'string',
+      },
+      virtualFiled_A1: {
+        type: 'string',
+        format: 'date',
+        default: '2025-01-01',
+      },
+      virtualFiled_A2: {
+        type: 'string',
+        format: 'date',
+      },
+      virtualField_B1: {
+        type: 'string',
+        format: 'week',
+        default: '2025-W28',
+      },
+      virtualField_B2: {
+        type: 'string',
+        format: 'week',
+      },
+    },
+    virtual: {
+      virtualField_A: {
+        fields: ['virtualFiled_A1', 'virtualFiled_A2'],
+      },
+      virtualField_B: {
+        fields: ['virtualField_B1', 'virtualField_B2'],
+      },
+    },
+    required: ['control', 'virtualField_A'],
+  } satisfies JsonSchema;
+
+  const [value, setValue] = useState<Record<string, unknown>>({});
+  const [errors, setErrors] = useState<JsonSchemaError[]>([]);
+
+  return (
+    <StoryLayout jsonSchema={jsonSchema} errors={errors} value={value}>
+      <Form
+        jsonSchema={jsonSchema}
+        defaultValue={{}}
+        onChange={setValue}
+        onValidate={setErrors}
+        validatorFactory={plugin.validator.compile}
+      />
+    </StoryLayout>
+  );
+};
+
 export const VirtualSchemaControlWithVisible = () => {
   const jsonSchema = {
     type: 'object',
