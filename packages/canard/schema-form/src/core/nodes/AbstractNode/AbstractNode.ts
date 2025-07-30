2c9a82ea6773c182796254a5256a6d2d67ae477d
Vincent K. Kelvin <lunox273@gmail.com>
2025-07-31 01:37:29 +0900

    Revert "[Refactor](schema-form): Optimize error handling in AbstractNode by using a more efficient loop structure"
    This reverts commit 82bebdcf0e6d2b5b149130c670d7d2116521e788.
    
    Revert "[Refactor](schema-form): Simplify error handling in AbstractNode by directly retrieving errors from the map"
    
    This reverts commit 18bab3b8df3e315e3323ecfeac4cb74b41c77b5d.

--- packages/canard/schema-form/src/core/nodes/AbstractNode/AbstractNode.ts
+++ packages/canard/schema-form/src/core/nodes/AbstractNode/AbstractNode.ts
@@ -765,21 +765,16 @@ export abstract class AbstractNode<
       errorsByDataPath.get(error.dataPath)?.push(error);
     }
 
-    const errorDataPaths = Array.from(errorsByDataPath.keys());
-
     // Find nodes by dataPath and set errors for child nodes as well
-    for (let i = 0, l = errorDataPaths.length; i < l; i++) {
-      const dataPath = errorDataPaths[i];
-      this.find(dataPath)?.setErrors(errorsByDataPath.get(dataPath)!);
-    }
+    for (const [dataPath, errors] of errorsByDataPath.entries())
+      this.find(dataPath)?.setErrors(errors);
 
     // Clear errors for nodes that had errors in previous error list but not in new error list
+    const errorDataPaths = Array.from(errorsByDataPath.keys());
     if (this.#errorDataPaths)
-      for (let i = 0, l = this.#errorDataPaths.length; i < l; i++) {
-        const dataPath = this.#errorDataPaths[i];
+      for (const dataPath of this.#errorDataPaths)
         if (!errorDataPaths.includes(dataPath))
           this.find(dataPath)?.clearErrors();
-      }
 
     // Update list of dataPaths with errors
     this.#errorDataPaths = errorDataPaths;
