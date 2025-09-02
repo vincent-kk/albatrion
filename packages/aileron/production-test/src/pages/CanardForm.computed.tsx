c29af149c8951951630314986e9d1f5ad13648eb
Vincent K. Kelvin <lunox273@gmail.com>
2025-09-03 02:51:47 +0900

    [Feature](Routing): Add new routes for CanardFormNullable and CanardFormComputed
    - 새로운 경로 `/canard-form-nullable` 및 `/canard-form-computed`를 추가하여 사용자가 nullable 및 computed 형태의 CanardForm을 탐색할 수 있도록 하였습니다.
    - Navigation 컴포넌트에 해당 경로에 대한 링크를 추가하여 사용자 인터페이스의 탐색성을 향상시켰습니다.
    - 이러한 변경 사항은 애플리케이션의 기능성을 확장하고 사용자 경험을 개선하는 데 기여합니다.

---
+++ packages/aileron/production-test/src/pages/CanardForm.computed.tsx
@@ -0,0 +1,82 @@
+import { useRef, useState } from "react";
+import { Form, type FormHandle, type JsonSchema } from "@canard/schema-form";
+import StoryLayout from "../components/StoryLayout";
+
+const schema = {
+  type: "object",
+  properties: {
+    category: {
+      type: "string",
+      enum: ["game", "movie"],
+      default: "game",
+    },
+    title: { type: "string" },
+    openingDate: {
+      type: "string",
+      format: "date",
+      "&visible": '../category === "game"',
+    },
+    releaseDate: {
+      type: "string",
+      format: "date",
+      "&visible": '../category === "movie"',
+    },
+    numOfPlayers: { type: "number" },
+    price: {
+      type: "number",
+      minimum: 50,
+    },
+  },
+} satisfies JsonSchema;
+
+const CanardForm = () => {
+  const [value, setValue] = useState<Record<string, unknown>>({});
+  const ref = useRef<FormHandle<typeof schema, Record<string, unknown>>>(null);
+  const revision = useRef(0);
+
+  return (
+    <div className="page">
+      <h1>Canard Form: computed</h1>
+      <button
+        onClick={() =>
+          ref.current?.setValue({
+            category: "movie",
+            title: "ABC",
+            openingDate: "1999-01-01",
+            releaseDate: "2020-02-01",
+            numOfPlayers: 10,
+            price: 100,
+          })
+        }
+      >
+        Set Movie
+      </button>
+      <button
+        onClick={() =>
+          ref.current?.setValue({
+            category: "game",
+            title: "DEF",
+            openingDate: "2030-02-01",
+            releaseDate: "2030-03-01",
+            numOfPlayers: 20,
+            price: 200,
+          })
+        }
+      >
+        Set Game
+      </button>
+      <StoryLayout jsonSchema={schema} value={value}>
+        <Form
+          jsonSchema={schema}
+          ref={ref}
+          onChange={(input) => {
+            console.log("onChange", revision.current++, input);
+            setValue(input as Record<string, unknown>);
+          }}
+        />
+      </StoryLayout>
+    </div>
+  );
+};
+
+export default CanardForm;
