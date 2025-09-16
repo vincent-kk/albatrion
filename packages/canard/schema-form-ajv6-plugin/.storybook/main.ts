aadf166958381423c73f2b6c8353467d0ed68f5f
Vincent K. Kelvin <lunox273@gmail.com>
2025-09-17 00:49:18 +0900

    [Chore](package.json, yarn.lock, .storybook): Upgrade Storybook dependencies to version 9.1.6
    - Updated Storybook-related dependencies in package.json and yarn.lock to version 9.1.6 for improved features and bug fixes.
    - Adjusted .storybook/main.ts files across multiple plugins to ensure consistent usage of the updated addon-docs.
    - Removed unused imports and streamlined the configuration for better maintainability.

--- packages/canard/schema-form-ajv6-plugin/.storybook/main.ts
+++ packages/canard/schema-form-ajv6-plugin/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { dirname, join, resolve } from 'path';
-import { mergeConfig } from 'vite';
+import { dirname, join } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -19,7 +18,7 @@ const config: StorybookConfig = {
   ],
   addons: [
     getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath("@storybook/addon-docs")
+    getAbsolutePath('@storybook/addon-docs'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
@@ -33,15 +32,6 @@ const config: StorybookConfig = {
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
       skipChildrenPropWithoutDoc: false,
     },
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      resolve: {
-        alias: {
-          '@/schema-form': resolve(__dirname, '../src'),
-        },
-      },
-    });
   },
 };
 export default config;
--- packages/canard/schema-form-antd-mobile-plugin/.storybook/main.ts
+++ packages/canard/schema-form-antd-mobile-plugin/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { dirname, join } from 'path';
-import { mergeConfig } from 'vite';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -19,7 +18,7 @@ const config: StorybookConfig = {
   ],
   addons: [
     getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath("@storybook/addon-docs")
+    getAbsolutePath('@storybook/addon-docs'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
@@ -33,9 +32,6 @@ const config: StorybookConfig = {
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
       skipChildrenPropWithoutDoc: false,
     },
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {});
   },
 };
 export default config;
