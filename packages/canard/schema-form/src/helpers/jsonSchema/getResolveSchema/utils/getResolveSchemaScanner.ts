9f6a09c34afdf3851f20925c2153087aa55d59e7
Vincent K. Kelvin <lunox273@gmail.com>
2025-09-21 15:52:14 +0900

    [Refactor](getResolveSchemaScanner): Enhance resolveReference function to handle base schema merging
    - Updated the resolveReference function to accept an additional entry parameter, allowing for improved handling of base schemas.
    - Implemented logic to merge base schemas with reference schemas when applicable, enhancing the functionality and maintainability of the code.
    - This change improves the clarity of the schema resolution process while preserving existing functionality.

--- packages/canard/schema-form/src/helpers/jsonSchema/getResolveSchema/utils/getResolveSchemaScanner.ts
+++ packages/canard/schema-form/src/helpers/jsonSchema/getResolveSchema/utils/getResolveSchemaScanner.ts
@@ -1,3 +1,5 @@
+import { isEmptyObject } from '@winglet/common-utils/filter';
+import { clone, merge } from '@winglet/common-utils/object';
 import { JsonSchemaScanner } from '@winglet/json-schema/scanner';
 
 import type { JsonSchema } from '@/schema-form/types';
@@ -8,7 +10,13 @@ export const getResolveSchemaScanner = (
 ) =>
   new JsonSchemaScanner({
     options: {
-      resolveReference: (path) => referenceTable.get(path),
+      resolveReference: (path, entry) => {
+        const { $ref: _, ...preferredSchema } = entry.schema;
+        const referenceSchema = referenceTable.get(path);
+        if (referenceSchema === undefined) return;
+        if (isEmptyObject(preferredSchema)) return referenceSchema;
+        return merge(clone(referenceSchema), preferredSchema);
+      },
       maxDepth,
     },
   });
