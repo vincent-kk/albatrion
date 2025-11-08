bf19b994aaa82baa296e187153801a99f96eddbd
Vincent K. Kelvin <lunox273@gmail.com>
2025-11-08 19:55:21 +0900

    [Refactor](modal): Enhance duration return type in useModalDuration hook
    - Updated the `useModalDuration` hook to explicitly type the duration return value as `Duration`, improving type safety and clarity.

--- packages/lerx/promise-modal/src/hooks/useModalDuration.ts
+++ packages/lerx/promise-modal/src/hooks/useModalDuration.ts
@@ -1,14 +1,19 @@
+import type { Duration } from '@aileron/declare';
+
 import type { ModalNode } from '@/promise-modal/core';
 import {
   useConfigurationDuration,
   useModalManager,
 } from '@/promise-modal/providers';
 
-export const useModalDuration = (id?: ModalNode['id']) => {
+export const useModalDuration = (modalId?: ModalNode['id']) => {
   const globalDuration = useConfigurationDuration();
-  if (id === undefined) return globalDuration;
-  const { modal } = useModalManager(id);
+  if (modalId === undefined) return globalDuration;
+  const { modal } = useModalManager(modalId);
   if (modal === undefined) return globalDuration;
   const milliseconds = modal.duration;
-  return { duration: milliseconds + 'ms', milliseconds };
+  return {
+    duration: (milliseconds + 'ms') as Duration,
+    milliseconds,
+  };
 };
