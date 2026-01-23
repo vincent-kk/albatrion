b45d8f12cb75c8f744a0a802493cc84e4798b517
Vincent K. Kelvin <lunox273@gmail.com>
2026-01-24 00:02:08 +0900

    Enhance schema mutation logic to include `injectValueTo` property
    - Updated the `mutate` function in `stripSchemaExtensions.ts` to check for the `injectValueTo` property in the schema, ensuring comprehensive schema validation and processing.
    - This change improves the handling of schema extensions by accounting for additional properties, enhancing the overall functionality of the schema form framework.

--- packages/canard/schema-form/src/helpers/jsonSchema/stripSchemaExtensions/stripSchemaExtensions.ts
+++ packages/canard/schema-form/src/helpers/jsonSchema/stripSchemaExtensions/stripSchemaExtensions.ts
@@ -20,7 +20,8 @@ const mutate: JsonScannerOptions<JsonSchemaWithVirtual>['mutate'] = ({
     schema.FormTypeInputProps === undefined &&
     schema.FormTypeRendererProps === undefined &&
     schema.errorMessages === undefined &&
-    schema.options === undefined
+    schema.options === undefined &&
+    schema.injectValueTo === undefined
   )
     return;
   const {
@@ -29,6 +30,7 @@ const mutate: JsonScannerOptions<JsonSchemaWithVirtual>['mutate'] = ({
     FormTypeRendererProps,
     errorMessages,
     options,
+    injectValueTo,
     ...stripedSchema
   } = schema;
   return stripedSchema;
