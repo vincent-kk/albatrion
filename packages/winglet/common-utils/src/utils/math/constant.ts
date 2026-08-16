ff43d7cccc51a23966883b4548a2866874bce632
Vincent K. Kelvin <lunox273@gmail.com>
2026-08-17 01:05:15 +0900

    fix(common-utils): stop gcd and lcm looping on non-finite and exponential inputs

--- packages/winglet/common-utils/src/utils/math/constant.ts
+++ packages/winglet/common-utils/src/utils/math/constant.ts
@@ -1,1 +1,4 @@
 export const BASE_36_DIGITS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+/** Largest fraction-digit count `Number.prototype.toFixed` accepts; beyond it the call throws `RangeError`. */
+export const MAX_FRACTION_DIGITS = 100;
